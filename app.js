6635bade210acc90129adea3a33da6b15aab222f
27qacem-crypto <27qacem@gmail.com>
2026-06-14 23:41:11 +0300

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx1NfQxB4sv0M2_m8d02Td1lgTgXg/exec"; // تأكد إن ده الرابط الجديد بالظبط
-
-// تأكد إن الجزء اللي فيه fetch بيستخدم المتغير ده بس:
-// fetch(WEB_APP_URL, { ... })
